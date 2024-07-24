f06dc91e7dc05fd7023402a867683eaf77bb17a1
Chaleira <pedromchaleira@gmail.com>
2024-07-24 20:29:35 +0100

    renamed file

--- vite/src/js/auxFts.js
+++ vite/src/js/auxFts.js
@@ -1,12 +1,15 @@
-export function sleep(us) {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            // Calculate remaining microseconds and wait in a busy loop
-            const start = performance.now();
-            while (performance.now() - start < us / 1000) {
-                // Busy-wait loop to handle the remaining microseconds
-            }
-            resolve();
-        }, Math.floor(us / 1000));
-    });
+// Checks collision between a rectangle and a circle
+export function isRectCircleCollision(circle, rect) {
+    const distX = Math.abs(circle.x - rect.x - rect.width / 2);
+    const distY = Math.abs(circle.y - rect.y - rect.height / 2);
+
+    if (distX > (rect.width / 2 + circle.radius)) { return false; }
+    if (distY > (rect.height / 2 + circle.radius)) { return false; }
+
+    if (distX <= (rect.width / 2)) { return true; } 
+    if (distY <= (rect.height / 2)) { return true; }
+
+    const dx = distX - rect.width / 2;
+    const dy = distY - rect.height / 2;
+    return (dx * dx + dy * dy <= (circle.radius * circle.radius));
 }
