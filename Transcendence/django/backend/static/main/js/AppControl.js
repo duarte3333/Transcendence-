be4327387d8c5022c4412e088e8437a53af83919
rubenstfj <rubensgomail@gmail.com>
2024-08-08 13:54:35 +0100

    fixed fetchapp, it throws error when not fetching, restructure code, cleaner

--- Transcendence/django/backend/static/main/js/AppControl.js
+++ Transcendence/django/backend/static/main/js/AppControl.js
@@ -26,100 +26,29 @@ export class AppControl {
         return (cookieValue);
     }
 
-    static  fetchElement(name) {
-        let success = this.fetchFromServer('api' + name).then(element => {
-            if (element == null) 
-                return (false);
-            document.body.appendChild(element);
-            return (this.executeScript().then(() => true));
-        }).catch(error => {
-            console.error('Error:', error);
-            return (false);
-        }); 
-        return (success);
-    }
-
-    // static async fetchApp(name) {
-    //     let success = this.fetchFromServer('api' + name).then(app => {
-    //         if (app == null) 
-    //             return (false);
-    //         // document.body.innerHTML = app;
-    //         document.body.appendChild(await app.text());
-    //         return (this.executeScript().then(() => {
-    //             // this.#removeAllScripts();
-    //             return (true);
-    //         }));
-    //     }).catch(error => {
-    //         console.error('Error:', error);
-    //         return (false);
-    //     });
-    //     return (success);
-    // }
-
-    // static async fetchApp(name) {
-    //     console.log("fetching");
-    //     let success = await fetch('api' + name)
-    //         .then(app => app.text())
-    //         .then(app => {
-    //             const newdiv = document.createElement('div');
-    //             newdiv.innerHTML = app;
-    //             newdiv.id = name;
-    //             this.executeScript(newdiv);
-    //             document.body.appendChild(newdiv);
-    //             return (true);
-    //         })
-    //         .catch(error => {
-    //             console.error('Error:', error);
-    //             return (false);
-    //         });
-    //     return (success);
-    // }
-
-    // static async fetchApp(name) {
-    //     console.log("fetching " + name);
-    //     return (
-    //         await fetch('api' + name)
-    //         .then(app => app.text())
-    //         .then(app => {
-    //             const newdiv = document.createElement('div');
-    //             newdiv.innerHTML = app;
-    //             newdiv.id = name;
-    //             document.body.appendChild(newdiv);
-    //             return (newdiv);
-    //         })
-    //         .then(newdiv => this.executeScript(newdiv))
-    //         .then(() => {return true})
-    //         .catch(error => {
-    //             console.error('Error:', error);
-    //             return (false);
-    //         })
-    //     );
-    // }
-
     static async fetchApp(name) {
-        console.log("fetching " + name);
-        try {
-            const response = await fetch('api' + name);
-            if (!response.ok) {
-                throw new Error('Network response was not ok: ' + response.statusText);
-            }
-            const app = await response.text();
+        let success = await fetch('api' + name).then(app => {
+            if (!app.ok)
+                throw new Error('Network response was not ok: ' + app.statusText);
+            app.text();
+        })
+        .then(app => {
             const newdiv = document.createElement('div');
             newdiv.innerHTML = app;
             newdiv.id = name;
+            newdiv.style.display = 'none';
             document.body.appendChild(newdiv);
-            
-            await this.executeScript(newdiv);
-            return true;
-        } catch (error) {
+            this.#executeScript(newdiv)
+            return (true);
+        })
+        .catch(error => {
             console.error('Error:', error);
-            return false;
-        }
+            return (false);
+        })
+        return (success);
     }
 
-
-    static async executeScript(doc) {
-        console.log("begin execution" + doc.id);
+    static async #executeScript(doc) {
         const scripts = doc.querySelectorAll('script');
         const scriptPromises = Array.from(scripts).map(script => {
             return new Promise((resolve, reject) => {
@@ -135,24 +64,24 @@ export class AppControl {
         return (await Promise.all(scriptPromises));
     }
 
-    static register() {
-        fetch("http://127.0.0.1:8000/api/register/", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-CSRFToken': this.getCookie('csrftoken')
-            },
-            body: JSON.stringify({
-                username: document.getElementById('username').value,
-                password: document.getElementById('password').value
-            })
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data.message);
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
-    };
 }
+// static register() {
+//     fetch("http://127.0.0.1:8000/api/register/", {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'X-CSRFToken': this.getCookie('csrftoken')
+//         },
+//         body: JSON.stringify({
+//             username: document.getElementById('username').value,
+//             password: document.getElementById('password').value
+//         })
+//     })
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data.message);
+//         })
+//         .catch(error => {
+//             console.error('Error:', error);
+//         });
+// };
